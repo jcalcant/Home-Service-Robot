304bf73aaaee50474f2e4908da9cf77eafaf8331
jcalcant <carlos.alcantarabeltran@gmail.com>
2020-10-07 12:59:22 +0000

    fix: refactored project for turtlebot. tested slam

---
+++ catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+xterm -e " cd $(pwd)/../..;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x -2.5 -y 2.05 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../my_robot/worlds/project4.world " &
+sleep 5
+xterm  -e  " cd $(pwd)/../..;
+source devel/setup.bash; 
+rosrun gmapping slam_gmapping " & 
+sleep 5
+xterm  -e  " cd $(pwd)/../..;
+source devel/setup.bash; 
+roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 5
+xterm  -e  " cd $(pwd)/../..;
+source devel/setup.bash; 
+roslaunch turtlebot_teleop keyboard_teleop.launch "
+
+
