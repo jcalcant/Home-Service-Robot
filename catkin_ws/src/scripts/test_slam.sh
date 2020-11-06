97f2b714e2a4aeec017ef2186bd206a1ad3b4da4
jcalcant <carlos.alcantarabeltran@gmail.com>
2020-11-06 18:45:47 +0000

    type: feat. Generated map

--- catkin_ws/src/scripts/test_slam.sh
+++ catkin_ws/src/scripts/test_slam.sh
@@ -3,13 +3,13 @@ xterm -e " cd $(pwd)/../..;
 source devel/setup.bash;
 export ROBOT_INITIAL_POSE='-x -6.5 -y 3.7 -z 0 -R 0 -P 0 -Y 0';
 roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=$(pwd)/../my_robot/worlds/HSR_project.world " &
-sleep 5
+sleep 20
 xterm  -e  " cd $(pwd)/../..;
 source devel/setup.bash;
-rosparam set /slam_gmapping/map_update_interval 0.2;
+rosparam set /slam_gmapping/map_update_interval 0.08;
 rosparam set /slam_gmapping/linearUpdate 0.05;
-rosparam set /slam_gmapping/angularUpdate 0.1;
-rosparam set /slam_gmapping/particles 100;
+rosparam set /slam_gmapping/angularUpdate 0.05;
+rosparam set /slam_gmapping/particles 200;
 rosparam set /slam_gmapping/srr 0.02;
 rosparam set /slam_gmapping/srt 0.02;
 rosparam set /slam_gmapping/str 0.02;
@@ -28,5 +28,6 @@ sleep 5
 xterm  -e  " cd $(pwd)/../..;
 source devel/setup.bash; 
 roslaunch turtlebot_teleop keyboard_teleop.launch "
+
 
 
